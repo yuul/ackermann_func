c76c55907106f24662646937597149648d26690f
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2018-02-11 00:09:12 -0600

    ackermann in ruby

---
+++ ackermann.rb
@@ -0,0 +1,9 @@
+def ackermann(m, n)
+  if m === 0
+    return n + 1
+  elsif m > 0 && n == 0
+    return ackermann m - 1, 1
+  elsif m > 0 && n > 0
+    return ackermann m - 1, ackermann(m, n - 1)
+  end
+end
