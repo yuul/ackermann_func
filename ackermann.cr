8f4e2aa95bc4517532bba8a9865070052ac03929
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2018-02-11 00:20:52 -0600

    Update ackermann.cr

--- ackermann.cr
+++ ackermann.cr
@@ -1,7 +1,7 @@
 def ackermann(m : Int32, n : Int32) : Int32
   if m === 0
     return n + 1
-  elsif m > 0 && n == 0
+  elsif m > 0 && n === 0
     return ackermann m - 1, 1
   elsif m > 0 && n > 0
     return ackermann m - 1, ackermann(m, n - 1)
