d14b506c08ccdc0798130464cc13b0ef356181ba
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2018-02-11 15:14:08 -0600

    Update ackermann.js

--- ackermann.js
+++ ackermann.js
@@ -1,8 +1,7 @@
-let ackermann = function(m, n) {
+let ackermann = (m, n) => {
   if (m === 0) {
-    n = n + 1;
-    return n;
-  } else if (m > 0 && n == 0) {
+    return n + 1;
+  } else if (m > 0 && n === 0) {
     return ackermann(m - 1, 1);
   } else if (m > 0 && n > 0) {
     return ackermann(m - 1, ackermann(m, n - 1));
