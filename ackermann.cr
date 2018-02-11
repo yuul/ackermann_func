2603b9f3f8fabea2c0342f753bec43cd977fda92
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2018-02-11 00:15:53 -0600

    Create ackermann.cr

---
+++ ackermann.cr
@@ -0,0 +1,12 @@
+def ackermann(m : Int32, n : Int32) : Int32
+  if m === 0
+    return n + 1
+  elsif m > 0 && n == 0
+    return ackermann m - 1, 1
+  elsif m > 0 && n > 0
+    return ackermann m - 1, ackermann(m, n - 1)
+  end
+  return 0
+end
+
+puts ackermann 4, 2
