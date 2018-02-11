841ff0e608fdd7f6f4ac3039f22692a7cebe4789
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2018-02-11 00:21:02 -0600

    Update ackermann.rb

--- ackermann.rb
+++ ackermann.rb
@@ -1,7 +1,7 @@
 def ackermann(m, n)
   if m === 0
     return n + 1
-  elsif m > 0 && n == 0
+  elsif m > 0 && n === 0
     return ackermann m - 1, 1
   elsif m > 0 && n > 0
     return ackermann m - 1, ackermann(m, n - 1)
