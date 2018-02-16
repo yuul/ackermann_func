efa452bc943097538a746c8d6151f3be5e4cb389
Ulysses Yu <ulyu1898@gmail.com>
2018-02-15 20:36:19 -0600

    Modified python to use console input

--- ackermann.py
+++ ackermann.py
@@ -6,4 +6,8 @@ def ackermann(m, n):
     elif m > 0 and n > 0:
         return ackermann(m-1, ackermann(m, n-1))
 
-print(ackermann(3, 3))
+
+num1 = int(input("First number of Ackermann: "))
+num2 = int(input("Second number of Ackermann: "))
+
+print("\n" + str(ackermann(num1, num2)))
